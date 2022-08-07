52ad4bad64e0affe9c74b7a44d10293d052880b4
Josh Levy <>
2022-08-07 18:16:19 -0400

    10 seconds min visit

--- constants/index.mjs
+++ constants/index.mjs
@@ -2,6 +2,6 @@ const isProd = process.env.NEXT_PUBLIC_VERCEL_ENV === "production";
 const LIVE = "https://www.joshlevy.io";
 const DEV = "http://localhost:3000";
 const BASE_URL = isProd ? LIVE : DEV;
-const MIN_PAGE_VIEW_TIME = 5;
+const MIN_PAGE_VIEW_TIME = 10;
 
 export { BASE_URL, isProd, LIVE, DEV, MIN_PAGE_VIEW_TIME };
