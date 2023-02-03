e4b24de343963752d64bbb3359c581267b6dcef0
Josh Levy <>
2023-02-03 09:35:54 -0500

    coding time seconds fix

--- utils/metrics.js
+++ utils/metrics.js
@@ -96,9 +96,11 @@ export const getWakaTimeStats = async () => {
     }
   );
   const json = await data.json();
-  const topProject = getTopProject(json["data"].map((el) => el.projects).flat())
+  const topProject = getTopProject(
+    json["data"].map((el) => el.projects).flat()
+  );
   return {
-    codingTime: Math.round(json["cummulative_total"].seconds / 3600) ?? "-",
+    codingTime: Math.round(json["cummulative_total"]?.seconds / 3600) ?? "-",
     languages:
       getTopLanguages(json["data"].map((el) => el.languages).flat()) || "-",
     project: (topProject !== "Desktop" && topProject) || "-"
